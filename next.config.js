aa6289fa336be38542a73f4b57b07ae5b5199552
Jeff Roche <jeff.roche@gmail.com>
2024-04-23 21:59:59 -0400

    Version test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    APP_VERSION: process.env.VERCEL_GIT_COMMIT_SHA,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+
+import { useEffect } from "react";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    console.log("Version info", {
+      version: process.env.APP_VERSION,
+      versionFromNext: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
